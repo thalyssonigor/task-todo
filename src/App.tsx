f2f8183d2de4cd7665cfd05f1772f319d8377162
Thalysson Igor <thalyssonigor@gmail.com>
2024-10-23 18:21:43 -0300

    feat: newproject

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Header } from "./header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="flex justify-between">
+        <h1>Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
